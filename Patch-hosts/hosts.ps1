1bc42e10c20a01acd38c38488bd0747c07416252
akirco <earlfx@163.com>
2023-05-15 18:16:41 +0800

    add hosts editor

---
+++ Patch-hosts/hosts.ps1
@@ -0,0 +1,120 @@
+$currentPath = $PSScriptRoot
+
+$sudo = Join-Path $currentPath "sudo.ps1"
+
+Import-Module $sudo
+
+$localhosts = "$env:windir\System32\drivers\etc\hosts"
+
+$localhostsBackup = Join-Path $currentPath "hosts"
+
+$localhostsBackupHash = Get-FileHash -Path $localhostsBackup
+
+$remoteHosts = "https://raw.githubusercontent.com/521xueweihan/GitHub520/main/hosts"
+
+
+
+function hosts {
+  [CmdletBinding(DefaultParameterSetName = 'cat')]
+  param(
+    [Parameter(ParameterSetName = 'cat')]
+    [Switch]$cat,
+
+    [Parameter(ParameterSetName = 'up')]
+    [Switch]$up
+  )
+
+  if ($cat) {
+    catHosts
+  }
+  elseif ($up) {
+    upHosts
+  }
+}
+
+
+
+function catHosts() {
+  $hostsContent = Get-Content $localhosts | ForEach-Object {
+    $line = $_.Trim()
+    if ($line -match '^(?<ip>[0-9\.]+)\s+(?<hostname>.+)$') {
+      [PSCustomObject]@{
+        IP        = $Matches['Ip']
+        Hostname  = $Matches['Hostname']
+        Commented = $false
+      }
+    }
+    elseif ($line -match '^\s*#') {
+      [PSCustomObject]@{
+        Line      = $line
+        Commented = $true
+      }
+    }
+    else {
+      [PSCustomObject]@{
+        Line = $line
+      }
+    }
+  }
+
+  $hostsContent | Where-Object { !$_.Commented } | Select-Object IP, Hostname
+}
+
+function upHosts() {
+  # 定义 loading 动画字符
+  # $loadingChars = "-\|/"
+  # $i = 0
+  # 打印 loading 动画，直到目标文件的哈希值与源文件的哈希值相同
+  Write-Host "Updating hosts file..."
+  while ($true) {
+    # 检查目标文件是否存在
+    if (Test-Path -Path $localhosts) {
+      # 获取目标文件的哈希值
+      $localhostsHash = Get-FileHash -Path $localhosts
+
+      # 比较两个哈希值是否相同
+      if ($localhostsHash.Hash -eq $localhostsBackupHash.Hash) {
+        break
+      }
+      else {
+        sudo.ps1 Copy-Item -Path $localhostsBackup -Destination $localhosts -Force
+      }
+    }
+
+    # 打印 loading 动画
+    # Write-Host -NoNewline "Copying $($loadingChars[$i++ % $loadingChars.Length])`r"
+    # Start-Sleep -Milliseconds 2000
+  }
+
+  # 打印成功消息
+  Write-Host "Hosts copied successfully." -ForegroundColor DarkCyan
+
+  Write-Host "Downloading remote hosts file..."
+
+  $response = Invoke-WebRequest $remoteHosts -UseBasicParsing
+
+  $hostsContent = $response.Content
+
+  Write-Host "Writing to hosts file..."
+
+  sudo.ps1 Add-Content $localhosts "$([Environment]::NewLine)"
+  sudo.ps1 Add-Content $localhosts $hostsContent
+
+  Write-Host "Hosts file updated successfully." -ForegroundColor DarkCyan
+
+  Invoke-Expression "ipconfig /flushdns"
+
+  $reader = New-Object System.IO.StringReader($hostsContent)
+
+  while ($true) {
+    $line = $reader.ReadLine()
+    if ($line -eq $null) {
+      break
+    }
+    if ($line.Contains("# Update time:")) {
+      $updateDate = $line.Replace("# Update time: ", "")
+      $time = Get-Date $updateDate
+      Write-Host "Update time: $($time.ToString('yyyy-MM-dd HH:mm:ss'))" -ForegroundColor DarkMagenta
+    }
+  }
+}
